62e59fef66e1a1f0b00a888ad998b90d9301540a
Gabriolini <janczakgabriel@gmail.com>
2017-06-16 18:46:06 +0100

    Edycja danych projektu 1.0
    -Zmiana Tytułu
    -Zzmina Opisu

--- application/views/separate_project.php
+++ application/views/separate_project.php
@@ -11,13 +11,16 @@
         echo "<b>Data otwarcia projektu: </b>".date("d/m/Y",$this->_ci_cached_vars["$i"]->creation_time)."<br/>";
         // Jeśli użytkownik jest twórcą projektu wyświetl przycisk usuwania projektu
         if($this->session->username == $this->_ci_cached_vars["$i"]->creator){
-            echo '<a  href="'.$this->_ci_cached_vars["$i"]->scenario_dir.'" download="">Scenariusz<a/><br/>'."</br>";
             $hidden = array('id_project' => $this->_ci_cached_vars["$i"]->id_project);
             echo form_open(base_url()."projekty/usun_projekt","",$hidden);
             echo form_submit('delate_project', 'Usuń projekt')."</br>";
 			echo form_close();
 			echo form_open(base_url()."projekty/id_projektu/".$this->_ci_cached_vars["$i"]->id_project,"");
 			echo form_submit('get_scenario', 'Ściągnij scenariusz')."</br>";
+			echo form_close();
+			echo form_open(base_url()."projekty/edytuj_projekt","",$hidden);
+			echo form_submit("edit_project", 'Edytuj projekt', $this->_ci_cached_vars["$i"]->id_project)."</br>";
+			
         }
     }
 
