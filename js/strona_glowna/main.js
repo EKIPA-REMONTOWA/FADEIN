e9a395b5bb21004c3186912ce8a5b23b545407ec
KGRozanski <kgrozanski@gmail.com>
2017-06-23 21:21:56 +0200

    v.1.1a
    -drobne poprawki

--- js/strona_glowna/main.js
+++ js/strona_glowna/main.js
@@ -3,13 +3,25 @@ function Set_search_status(arg) {
 	if (arg == 1) {
 		$(search_2).removeClass("active");
 		$(search_1).addClass("active");
-		$(search_2).attr('value', '0');
-		$(search_1).attr('value', '1');
+		$(user_proj).attr('value', '0');
+		$(sel1).empty();
+		var opcjeSzukaniaUser = ["Nazwa użytkownika", "Profesja", "Imię", "Nazwisko"];
+		opcjeSzukaniaUser.forEach(listuj);
 	}
 	else if (arg == 2) {
 		$(search_1).removeClass("active");
 		$(search_2).addClass("active");
-		$(search_1).attr('value', '0');
-		$(search_2).attr('value', '1');
+		$(user_proj).attr('value', '1');
+		$(sel1).empty();
+		var opcjeSzukaniaProj = ["Tytuł", "Twórca", "Gatunek"];
+		opcjeSzukaniaProj.forEach(listuj);
 	}
+	
+	function listuj(item, index) {
+			var opcja = document.createElement('option');
+			var txt = document.createTextNode(item);
+			opcja.setAttribute('value', index);
+			opcja.appendChild(txt);
+			$(sel1).append(opcja);
+		}
 }
