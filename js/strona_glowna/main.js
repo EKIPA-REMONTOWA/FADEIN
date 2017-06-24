3756f923478f5241fda91f44dbe5dc5deb70eaa9
KGRozanski <kgrozanski@gmail.com>
2017-06-24 16:55:39 +0200

    v.1.4.1-a
    -poprawka błędów wyszukiwania "zmienna szukania"

--- js/strona_glowna/main.js
+++ js/strona_glowna/main.js
@@ -3,13 +3,16 @@ function Set_search_status(arg) {
 	if (arg == 1) {
 		$(search_2).removeClass("active");
 		$(search_1).addClass("active");
+		$(zSzukania).attr('value',0);
 		$(sel1).empty();
+		$(zSzukania).attr('value',0);
 		var opcjeSzukaniaUser = ["Nazwa użytkownika", "Profesja", "Imię", "Nazwisko"];
 		opcjeSzukaniaUser.forEach(listuj);
 	}
 	else if (arg == 2) {
 		$(search_1).removeClass("active");
 		$(search_2).addClass("active");
+		$(zSzukania).attr('value',1);
 		$(sel1).empty();
 		var opcjeSzukaniaProj = ["Tytuł", "Twórca", "Gatunek"];
 		opcjeSzukaniaProj.forEach(listuj);
